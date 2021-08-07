f9be2cff72813b00f0cf84acc335a80e5b0f0210
Ipsita-dhar <64036822+Ipsita-dhar@users.noreply.github.com>
2021-08-08 02:25:52 +0530

    LetsUpgrade Javascript

---
+++ Proj_2(1)/second.js
@@ -0,0 +1,19 @@
+let a=12;
+
+while(a<=10)
+{
+    console.log(a);
+    a++;
+    
+}
+
+
+let arr=["Ipsita","Kaustav","Preeshita","Shalini", "Debosmit"];
+
+console.log(arr.length);
+
+for(let i=arr.length;i>=0;i--){
+
+    console.log(arr[i],i);
+
+}
